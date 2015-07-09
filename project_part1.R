bb73fed80e5bb53d7477093eaa952c66effe81c9
Zhengshi Lucia <zhengshizhao917@gmail.com>
2015-07-09 00:25:45 -0400

    Modify plot for comparing sample mean, variance to those in theoretical distribution. Add number of simulation for part3 to demonstrate the mean distribution is closed to normal distribution.

--- project_part1.R
+++ project_part1.R
@@ -1,53 +1,62 @@
-
-#sim_exp<- rexp(1000, 0.2)
+### sample means
 set.seed(10302)
 sim_exp_means = NULL
 for (i in 1 : 1000) sim_exp_means = c(sim_exp_means, mean(rexp(40, 0.2)))
-hist(sim_exp_means,freq=FALSE)
-abline(v=mean(sim_exp_means),col="red")
-abline(v=5.0,col="green" )
 
-x1<-sim_exp_means
-y1<- dnorm(x1, mean=5, sd=5)
-plot(x1, y1)
-
-
-
-
-sim_exp_mean <-mean(sim_exp_means)
-sim_exp_sd <-sd(sim_exp_means)
-sim_exp_mean;sim_exp_sd
+mean(sim_exp_means)
+sd(sim_exp_means)
 
 x1<-sort(sim_exp_means)
-y1<- dnorm(x1, mean=5, sd=0.8)
-data_norm <- cbind(x1,y1)
-head(data_norm)
-plot(data_norm[,1], data_norm[,2],type="l")
+y1<- dnorm(x1, mean=mean(sim_exp_means), sd=sd(sim_exp_means))
 
-hist(sim_exp_means,freq=FALSE)
-lines(data_norm[,1], data_norm[,2])
-abline(v=mean(sim_exp_means),col="red", lwd=3)
-abline(v=5.0,col="green",lwd=3 )
+hist(sim_exp_means,freq=FALSE,col="lightblue",breaks=40, xlab="Sample means")
+abline(v=5.0,col="red",lwd=4)
+lines(x1,y1,type="l",lwd=2)
 
-
+### sample variances
 set.seed(10302)
 sim_exp_vars = NULL
 for (i in 1 : 1000) sim_exp_vars = c(sim_exp_vars, var(rexp(40, 0.2)))
 
-
 x2<-sort(sim_exp_vars)
 y2<- dnorm(x2, mean=mean(sim_exp_vars), sd=sd(sim_exp_vars))
-plot(x2,y2,type="l")
+data_var_norm <- cbind(x2,y2)
 
-hist(sim_exp_vars,freq=FALSE)
+hist(sim_exp_vars,freq=FALSE,breaks=40,col="lightblue",xlab="Sample variances")
 lines(x2,y2)
-abline(v=mean(sim_exp_vars),col="red")
-abline(v=25.0,col="green" )
-mean(sim_exp_vars)
+lines(density(sim_exp_vars))
+abline(v=25.0,col="red", lwd=4 )
+
+#distributions
 
 
-sim_exp<- rexp(10000, 0.2)
+#sim_exp <- rexp(1000,.2)
+set.seed(10033)
+sim_exp <- rexp(1000,.2)
+x3 <- sort(runif(1000,min=0,max=40))
+y3<-dexp(x3, rate = .2, log = FALSE)
+par(mfrow=c(1,2))
+hist(sim_exp, freq=FALSE,breaks=40,col="lightblue")
+lines(x3,y3,type='l')
 
-hist(sim_exp, freq=FALSE)
-lines(density(sim_exp))
-hist(sim_exp_means)
+## 
+set.seed(10302)
+sim_exp_means = NULL
+for (i in 1 : 1000) sim_exp_means = c(sim_exp_means, mean(rexp(40, 0.2)))
+par(mfrow=c(1,2))
+hist(sim_exp_means,freq=FALSE,col="lightblue",breaks=40, xlab="Sample means")
+abline(v=5.0,col="red",lwd=4)
+abline(v=mean(sim_exp_means),col="green",lwd=4)
+lines(density(sim_exp_means))
+lines(x1,y1,type="l",lwd=2)
+
+for (i in 1 : 10000) sim_exp_means = c(sim_exp_means, mean(rexp(40, 0.2)))
+fit<-density(sim_exp_means)
+yy <- rnorm(1000, sample(sim_exp_means, size = 1000, replace = TRUE), fit$bw)
+hist(sim_exp_means,freq=FALSE,col="lightblue",breaks=40, xlab="Sample means")
+lines(fit)
+lines(density(yy), col = "blue")
+lines(x1,y1,type="l",lwd=2)
+
+
+
